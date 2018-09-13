2648a94bbcd1c48a2d9671625d6ba257b8b6d0a6
maccallum <john.m@ccallum.com>
2018-09-13 23:27:18 +0200

    cleaned up x.dist.cpp

--- source/x.dist.cpp
+++ source/x.dist.cpp
@@ -29,20 +29,6 @@ void *xdist_new(t_symbol *msg, short argc, t_atom *argv)
 	char buf[128];
 	sprintf(buf, "x.dist.%s", atom_getsym(argv)->s_name);
 	return object_new_typed(CLASS_BOX, gensym(buf), argc - 1, argv + 1);
-		// x->outlet_delegation = outlet_new((t_object *)x, NULL);
-		// x->outlet_main = outlet_new((t_object *)x, NULL);
-		// t_object *conduit = object_findregistered(ps_xdist_conduit, atom_getsym(argv));
-		// if(!conduit){
-		// 	t_atom a[2];
-		// 	atom_setsym(a, gensym(XDIST_REGISTER_ATTR));
-		// 	atom_setlong(a + 1, 1);
-			
-		// 	object_new(CLASS_NOBOX, ps_xdist_conduit, atom_getsym(argv));
-		// 	conduit = object_findregistered(ps_xdist_conduit, atom_getsym(argv));
-		// }
-		// if(conduit){
-		// 	x->conduit = (t_xdist_conduit *)conduit;
-		// }
 }
 
 void ext_main(void *r)
@@ -50,14 +36,4 @@ void ext_main(void *r)
 	t_class *c;
 	c = class_new("x.dist", (method)xdist_new, NULL, sizeof(t_maxobj), 0L, A_GIMME, 0);
 	class_register(CLASS_BOX, c);
-
-	// class_addmethod(c, (method)xdist_int,	"int", A_LONG, 0);
-	// class_addmethod(c, (method)xdist_float, "float", A_FLOAT, 0);
-	// class_addmethod(c, (method)xdist_anything, "list", A_GIMME, 0);
-	// class_addmethod(c, (method)xdist_anything, "anything", A_GIMME, 0);
-	// class_addmethod(c, (method)xdist_assist, "assist", A_CANT, 0);
-	// class_addmethod(c, (method)xdist_notify, "notify", A_CANT, 0);
-
-	// class_register(CLASS_BOX, c);
-	// xdist_class = c;
 }
